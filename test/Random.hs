5df132e857f7173b9f5c2192c4c938adf0f9756f
LATBauerdick <github@latbauerdick.de>
2016-12-07 15:18:57 -0600

    hist of randomized fitted mass distribution

--- test/Random.hs
+++ test/Random.hs
@@ -7,7 +7,7 @@ import Data.Random.Normal
 
 import Data.List ( foldl', unfoldr, mapAccumL, (!!) )
 
-import Types ( XMeas (..), VHMeas (..), HMeas (..), Prong (..)
+import Types ( XMeas (..), VHMeas (..), HMeas (..), Prong (..), MMeas (..)
              , showXMeas, showMMeas, q2p, v3,l3,v5,l5 )
 import Coeff ( invMass )
 import Matrix ( toList, fromList, chol, scalar )
@@ -15,6 +15,23 @@ import Fit ( fit )
 
 import qualified Graphics.Gnuplot.Frame.OptionSet as Opts
 import Graphics.Histogram
+
+-- Function to process our random sequence
+process :: [(Double, Double)] -> (Int, Int)
+process = foldl' sumInCircle (0, 0)
+
+-- Function to process a running value and a random value, producing a new running value.
+sumInCircle :: (Int, Int) -> (Double, Double) -> (Int, Int)
+sumInCircle (!ins, !total) (x, y) = (ins + if x*x + y*y < 1.0 then 1 else 0,
+                               total + 1)
+
+-- Function to display the running value.
+display:: (Int, Int) -> String
+display (heads, coins) = "π = "  ++ (show $ 4.0 * fromIntegral heads / fromIntegral coins)
+
+-- function to prepare the random sequence for processing
+prep :: [Double] -> [(Double, Double)]
+prep (a:b:r) = (a,b):prep r
 
 -- return randomized XMeas vector according to its covariance matrix,
 -- using -> 3 normal distributed random numbers
@@ -24,7 +41,8 @@ randV (XMeas v vv) rnd = XMeas v' vv where
   v'  = v3 $ zipWith (+) (l3 v) (l3 (chol vv * v3 rnd))
 
 randVH :: VHMeas HMeas -> [Double] -> (VHMeas HMeas, [Double])
-randVH (VHMeas vm hl) rs = (VHMeas (randV vm rsv) hl', rsf) where
+randVH (VHMeas vm hl) rs = (VHMeas vm' hl', rsf) where
+  vm' = vm --randV vm rsv
   (rsv, rsh) = splitAt 3 rs
   (rsf, hl') = mapAccumL randH rsh hl
 
@@ -44,33 +62,39 @@ dr v f (r0:r1:r2:rs) = Just (f (randV v [r0,r1,r2]), rs)
 getVx :: XMeas -> Double
 getVx (XMeas v _) = (!!) (l3 v) 2
 
+drs' :: VHMeas HMeas -> (VHMeas HMeas -> Double) -> [Double] -> [Double]
+drs' vh f rs = unfoldr (dr' vh f) rs
+
+dr' :: VHMeas HMeas -> (VHMeas HMeas -> Double) -> [Double] -> Maybe (Double, [Double])
+dr' vh f rs = Just (f (vh'), rs') where
+  (vh', rs') = randVH vh rs
+
+fm :: VHMeas HMeas -> Double
+fm (VHMeas v hl) = m where
+  Prong _ _ ql _ = fit v hl
+  MMeas m _ = invMass $ map q2p ql
+
 rp :: Int -> VHMeas HMeas -> IO ()
-rp _cnt (VHMeas v hl) = do
+rp cnt (VHMeas v hl) = do
   g <- newStdGen
+  putStrLn . display . process . take cnt . prep $  normals g
+
+  let hist = histogram binSturges $ take cnt (drs' (VHMeas v hl) fm (normals g))
+  plot "vz.png" hist
+  let Prong _ _ ql _ = fit v hl
+  showMMeas "Inv Mass " $ invMass $ map q2p ql
+
   -- putStrLn $ showXMeas "input  " v
   -- let v' = randV v $ take 3 ( normals g )
   -- putStrLn $ showXMeas "smeared" v'
+  --
+  -- let (VHMeas _ hl', rnd) = randVH (VHMeas v hl) (normals g)
+  -- let Prong _ _ ql _ = fit v hl'
+  -- showMMeas "Inv Mass " $ invMass $ map q2p ql
+  -- let (VHMeas _ hl', rnd') = randVH (VHMeas v hl) rnd
+  -- let Prong _ _ ql _ = fit v hl'
+  -- showMMeas "Inv Mass " $ invMass $ map q2p ql
+  -- let (VHMeas _ hl', rnd'') = randVH (VHMeas v hl) rnd'
+  -- let Prong _ _ ql _ = fit v hl'
+  -- showMMeas "Inv Mass " $ invMass $ map q2p ql
 
-  let hist = histogram binSturges imd
-  plot "invMass.png" hist
-
-  let (VHMeas _ hl', rnd) = randVH (VHMeas v hl) (normals g)
-  let Prong _ _ ql _ = fit v hl'
-  showMMeas "Inv Mass " $ invMass $ map q2p ql
-  let (VHMeas _ hl', rnd') = randVH (VHMeas v hl) rnd
-  let Prong _ _ ql _ = fit v hl'
-  showMMeas "Inv Mass " $ invMass $ map q2p ql
-  let (VHMeas _ hl', rnd'') = randVH (VHMeas v hl) rnd'
-  let Prong _ _ ql _ = fit v hl'
-  showMMeas "Inv Mass " $ invMass $ map q2p ql
-
-imd = [1494.8, 1491.9, 1492.1, 1494.2, 1494.5, 1497.0, 1499.0, 1494.9, 1494.8
-  , 1497.3, 1494.8, 1499.3, 1492.5, 1492.2, 1502.3, 1489.6, 1494.8, 1494.0
-  , 1489.5, 1497.5, 1499.5, 1497.1, 1490.2, 1496.5, 1494.8, 1494.6, 1498.0
-  , 1493.8, 1490.5, 1490.4, 1496.4, 1486.7, 1494.8, 1490.0, 1500.1, 1497.1
-  , 1495.8, 1494.9, 1490.4, 1491.5, 1494.8, 1501.7, 1495.2, 1492.5, 1494.8
-  , 1489.1, 1491.0, 1491.5, 1494.8, 1501.5, 1494.9, 1499.9, 1494.8, 1494.1
-  , 1493.3, 1497.5, 1494.8, 1495.7, 1496.1, 1498.7, 1494.8, 1495.2, 1503.6
-  , 1489.5, 1494.8, 1494.7, 1491.8, 1498.7, 1494.8, 1490.7, 1501.8, 1502.3
-  , 1494.8, 1494.5, 1493.6, 1495.4
-  ]
