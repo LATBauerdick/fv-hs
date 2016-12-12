421193d0fb8994352d43b135f88a9891b9ed76b3
LATBauerdick <github@latbauerdick.de>
2016-12-11 23:57:29 -0600

    re-factoring fit'

--- test/Random.hs
+++ test/Random.hs
@@ -43,11 +43,12 @@ randV :: XMeas -> [Double] -> XMeas
 randV (XMeas v vv) rnd = XMeas v' vv where
   v'  = v3 $ zipWith (+) (l3 v) (l3 (chol vv * v3 rnd))
 
--- randomize a single helix parameters measurment, based on the cov matrix
--- return randomized helix and "remaining" random numbers
-randH :: [Double] -> HMeas -> ([Double], HMeas)
-randH (r0:r1:r2:r3:r4:rs) (HMeas h hh w0) = (rs, HMeas h' hh w0) where
-  h' = v5 $ zipWith (+) (l5 h) (l5 (chol hh * v5 [r0,r1,r2,r3,r4]))
+-- calc fitted invariant mass of VHMeas
+fitMass :: VHMeas -> Double
+fitMass vm = m where
+  Prong _ _ ql _ = fit vm
+  (MMeas m _) = invMass . map q2p $ ql
+
 
 {-
 VHMeas v hl <- hSlurp thisFile
@@ -71,17 +72,17 @@ randomize :: VHMeas -> (VHMeas -> Double) -> [Double] -> Maybe (Double, [Double]
 randomize vh f rs = Just (f vh', rs') where
   (vh', rs') = randVH vh rs
 
--- calc fitted invariant mass of VHMeas
-fitMass :: VHMeas -> Double
-fitMass vm = m where
-  Prong _ _ ql _ = fit vm
-  (MMeas m _) = invMass . map q2p $ ql
-
 -- randomize the helices in the supplied VHMeas
 -- and return randomized VHMeas and remaining randoms list
 randVH :: VHMeas -> [Double] -> (VHMeas, [Double])
 randVH (VHMeas v hl) rs = (VHMeas v hl', rs') where
   (rs', hl') = mapAccumL randH rs hl
+
+-- randomize a single helix parameters measurment, based on the cov matrix
+-- return randomized helix and "remaining" random numbers
+randH :: [Double] -> HMeas -> ([Double], HMeas)
+randH (r0:r1:r2:r3:r4:rs) (HMeas h hh w0) = (rs, HMeas h' hh w0) where
+  h' = v5 $ zipWith (+) (l5 h) (l5 (chol hh * v5 [r0,r1,r2,r3,r4]))
 
 -- create a list of values for histogramming, randomizing the supplied VHMeas
 -- using the list of normaly distributed random numbers
