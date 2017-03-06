d0e3c9e2ca642fdddf85d29db09afd3bc72dfa8b
LATBauerdick <git@bauerdick.org>
2017-03-06 12:30:07 -0600

    restructure modules to have FVT

---
+++ test/FVT.hs
@@ -0,0 +1,26 @@
+-- file: test.hs
+--
+module Main ( main ) where
+
+import Prelude
+import System.Environment
+import System.Exit
+
+import FVT.Test ( test )
+
+main :: IO ()
+main = getArgs >>= parse
+
+parse :: [String] -> IO ()
+parse ["-h"] = usage   >> exit
+parse ["-v"] = version >> exit
+parse []     = test ["1"]
+parse args   = test args
+
+usage :: IO ()
+usage   = putStrLn "Usage: fvt [-vh] [test# ..]"
+version :: IO ()
+version = putStrLn "Haskell fvt 0.1"
+exit :: IO ()
+exit    = exitSuccess
+
