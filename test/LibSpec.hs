78a20a376f735049aa95c1b063fcddb33eebdc57
LATBauerdick <git@bauerdick.org>
2017-03-09 14:50:11 -0600

    works now with new FVT structure and --nix

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,17 +1,21 @@
 module LibSpec where
 
 import Test.Hspec
-import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Control.Exception (evaluate)
 
-import Lib (ourAdd)
+import FVT.Input (hSlurp)
+import FV.Types ( VHMeas (..), HMeas (..) )
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec =
-  describe "Lib" $ do
+  describe "FVT.Test" $ do
     it "works" $ do
-      True `shouldBe` True
-    prop "ourAdd is commutative" $ \x y ->
-      ourAdd x y `shouldBe` ourAdd y x
+      VHMeas _ hl <- hSlurp "dat/tav-0.dat"
+      let HMeas _ _ w = head hl
+      w `shouldBe` 1.0
+--    prop "ourAdd is commutative" $ \x y ->
+--      ourAdd x y `shouldBe` ourAdd y x
