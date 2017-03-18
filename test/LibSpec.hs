2ada3ca32ed7e7a3c827b730fca839f1fbc929ba
LATBauerdick <git@bauerdick.org>
2017-03-18 16:01:45 -0500

    added mc truth info

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -52,7 +52,7 @@ spec =
 
     describe "FVT.Test" $ do
       it "doCluster works" $ do
-        (vm, _) <- hSlurp "dat/tav-0.dat"
+        (vm, _) <- hSlurp "dat/tav-4.dat"
         _ <- doCluster vm
         (1 :: Int) `shouldBe` 1
 
